c1581c7c367d0b29d3093717f76ebf4cb4425e7f
Siddharth Gupta <siddharth11tha@gmail.com>
2024-02-20 14:15:33 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './index.css'
-import Navbar from './components/navbar/navbar.jsx'
+import Navbar from './components/Navbar/navbar.jsx'
 import Footer from './components/footer/footer.jsx'
 import Hero from './components/Hero/Hero.jsx'
-import Mens from './components/mens/mens.jsx'
-import Women from './components/women/women.jsx'
-import Kids from './components/kids/kids.jsx'
+import Mens from './components/Mens/Mens.jsx'
+import Women from './components/Women/Women.jsx'
+import Kids from './components/Kids/Kids.jsx'
 import Cart from './components/cart/cart.jsx'
 
 
